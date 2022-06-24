a1fe0d6ea789e5a6d6d4af12fa8a8d1666497269
Marco <marco.almeida@ua.pt>
2022-06-24 01:16:15 +0100

    fix

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,17 +1,9 @@
 function validar() {
-    var email = document.getElementById("email").value;
+    var utilizador = document.getElementById("utilizador").value;
     var password = document.getElementById("password").value;
     var mensagem = "";
-    const validateEmail = (email) => {
-        return String(email)
-            .toLowerCase()
-            .match(
-                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-            );
-    };
-
-    if (!validateEmail(email)) {
-        mensagem += "Email inválido!<br>";
+    if (utilizador.length == 0) {
+        mensagem += "Nome de utilizador inválido!<br>";
     }
     if (password.length < 8) {
         mensagem += "Palavra-chave deve ter pelo menos 8 carateres!<br>";
@@ -20,5 +12,6 @@ function validar() {
     if (mensagem == "") {
         window.location = "stock.html";
     }
+
     document.getElementById("message").innerHTML = mensagem;
 }
